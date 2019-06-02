4f620a5937bc60cb14369c9e2c9abe566f88ed91
Kevin Lynch <kevin@technical.ly>
2019-06-02 14:47:02 -0400

    Set up Heroku deployment

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
 
-console.log(process.env.API_TOKEN)
-
 const app = express()
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan('dev'))
+app.use(morgan(morganSetting))
+app.use(cors())
+app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
@@ -18,11 +21,17 @@ app.use(function validateBearerToken(req, res, next) {
     }
     // move to the next middleware
     next()
-  })
+})
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 function handleGetTypes(req, res) {
+    res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
     let response = POKEDEX.pokemon;
 
   // filter our pokemon by name if name query param is present
@@ -43,15 +52,19 @@ function handleGetTypes(req, res) {
   res.json(response)
 }
 
-app.get('/types', handleGetTypes)
-
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
-}
-
 app.get('/pokemon', handleGetPokemon)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
